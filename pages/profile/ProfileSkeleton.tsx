e8540b690d1f93e5d8c3453cae3c7f822b6969bf
VirtuuX <somargame22@gmail.com>
2022-11-09 20:28:44 +0600

    fourth commit

--- pages/profile/ProfileSkeleton.tsx
+++ pages/profile/ProfileSkeleton.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './profile.module.scss';
 import { Skeleton } from '@material-ui/lab';
+import { useMediaQuery } from '@material-ui/core';
 
 const ProfileSkeleton = () => {
+  const matches427 = useMediaQuery('(max-width:427px)');
+
   return (
     <div className={styles.skeleton}>
       <div className={styles.skeletonContent}>
@@ -12,6 +15,12 @@ const ProfileSkeleton = () => {
           </div>
           <div className={styles.skeletonInfo}>
             <Skeleton variant="text" width={120} height={30} style={{ marginTop: 10 }} />
+            <Skeleton
+              variant="rect"
+              width={45}
+              height={42}
+              style={{ borderRadius: 8, display: matches427 ? 'block' : 'none', marginBottom: 20, marginTop: 10 }}
+            />
             <Skeleton variant="text" width={180} height={20} />
           </div>
           <div className={styles.skeletonTabs}>
